b3e5f0a309f926200023903a40e0c8ff223b6be8
Marco-678 <marco.gallina678@gmail.com>
2025-06-12 16:58:09 +0200

    enemy5, sky, hb brutta

--- scripts/bullet_enemy.gd
+++ scripts/bullet_enemy.gd
@@ -4,9 +4,10 @@ const EXPLOSION = preload("res://scenes/explosionpro.tscn")
 
 var speed = 600
 
-func _physics_process(delta: float) -> void:
-	position.y += speed * delta
 
+func _physics_process(delta: float) -> void:
+	
+	position.y += speed * delta
 
 
 func _on_area_entered(area: Area2D) -> void:
