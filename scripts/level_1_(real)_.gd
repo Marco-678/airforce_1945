6dbe7568f00b30e4cf7a08bfefc1e522afdd115c
albycuoio <albizanna08@gmail.com>
2025-06-16 10:43:26 +0200

    BO

--- scripts/level_1_(real)_.gd
+++ scripts/level_1_(real)_.gd
@@ -5,10 +5,17 @@ var delay = 2
 var delay2 = 3
 var enemy1
 var enemy3
-var enemy4
+var enemy5
+var enemy2
+var delay3 = 2.5
 const pl = preload("res://scenes/enemy_1.tscn")
 const pl3 = preload("res://scenes/enemy_3.tscn")
-const pl4 = preload("res://scenes/enemy_5.tscn")
+const pl5 = preload("res://scenes/enemy_5.tscn")
+const pl2 = preload("res://scenes/enemy_2.tscn")
+const pl4 = preload("res://scenes/enemy_4.tscn")
+
+var enemy4
+
 
 
 
@@ -29,11 +36,32 @@ func _process(delta: float) -> void:
 		get_tree().current_scene.add_child(enemy3)
 		
 		
+
+	if DELTA > 2 and DELTA < 22 and $Timer.is_stopped():
+		$Timer.start(delay2)
+		var enemy5 = pl5.instantiate()
+		enemy5.position = position
+		get_tree().current_scene.add_child(enemy5)
+		
+	if DELTA > 36 and DELTA < 42 and $Timer.is_stopped():
+		$Timer.start(delay3)
+		var enemy2 = pl2.instantiate()
+		enemy2.position = position
+		get_tree().current_scene.add_child(enemy2)
+		
+	if DELTA > 32 and DELTA < 47 and $Timer.is_stopped():
+		$Timer.start(delay)
+		var enemy4 = pl4.instantiate()
+		enemy4.position = position
+		get_tree().current_scene.add_child(enemy4)
+
 	if DELTA > 10 and DELTA < 25 and $Timer.is_stopped():
+
 		$Timer.start(delay2)
 		var enemy4 = pl4.instantiate()
 		enemy4.position = position
 		get_tree().current_scene.add_child(enemy4)
+
 		
 		
 	if DELTA > 22 and DELTA < 25 and $Timer.is_stopped():
