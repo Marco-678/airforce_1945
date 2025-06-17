79ac389d1a60d1bd9c381d91fe42f01e6268037f
albycuoio <albizanna08@gmail.com>
2025-06-17 09:21:16 +0200

    bug fixes

--- scripts/bullet_leftenemy.gd
+++ scripts/bullet_leftenemy.gd
@@ -9,8 +9,12 @@ func _physics_process(delta2: float) -> void:
 	if position.x >= -60:
 		position.x -= 10
 		position.y += 20
-	
-
+		
+func _on_area_entered(area: Area2D) -> void:
+	var explosion = EXPLOSION.instantiate()
+	explosion.global_position = global_position
+	get_tree().current_scene.add_child(explosion)
+	queue_free()
 	
 
 
