b125fa25bc742e89de31a19f453a9a77d030395e
Marco-678 <marco.gallina678@gmail.com>
2025-06-18 10:37:04 +0200

    healthbar

--- scripts/Bullet/bullet_enemy_2.gd
+++ scripts/Bullet/bullet_enemy_2.gd
@@ -8,7 +8,9 @@ func _physics_process(delta2: float) -> void:
 
 	position.y += speed2 * delta2
 
-
+	if not(position.x > 0 and position.x < 600 and position.y > 0 and position.y < 900):
+		queue_free()
+	
 func _on_area_entered(area: Area2D) -> void:
 	var explosion = EXPLOSION.instantiate()
 	explosion.global_position = global_position
